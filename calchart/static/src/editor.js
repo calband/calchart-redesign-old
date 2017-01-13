42bcc6b1b620def6bb1966effa9e1e7fb014fd4b
Brandon Chinn <brandonchinn178@gmail.com>
2017-01-12 22:25:33 -0800

    convert editor.js

--- calchart/static/src/editor.js
+++ calchart/static/src/editor.js
@@ -1,48 +1,50 @@
-var EditorController = require("calchart/controllers/EditorController");
-var Grapher = require("calchart/Grapher");
-var Show = require("calchart/Show");
-var UIUtils = require("utils/UIUtils");
+import * as _ from "lodash";
+
+import EditorController from "calchart/controllers/EditorController";
+import Show from "calchart/Show";
+
+import { getData, hidePopup, showError, showPopup } from "utils/UIUtils";
 
 /**
  * Setup show, prompting user for show details if the show is new
  */
-$(document).ready(function() {
-    if (window.show !== null) {
-        var show = new Show(window.show);
-        var controller = EditorController.init(show);
+$(function() {
+    if (!_.isNull(window.show)) {
+        let show = new Show(window.show);
+        EditorController.init(show);
         return;
     }
 
-    UIUtils.showPopup("setup-show", {
+    showPopup("setup-show", {
         onSubmit: function(popup) {
-            var data = UIUtils.getData(popup);
+            let data = getData(popup);
 
             // validate data
 
             if (data.num_dots === "") {
-                UIUtils.showError("Please provide the number of dots in the show.");
+                showError("Please provide the number of dots in the show.");
                 return;
             }
 
             data.num_dots = parseInt(data.num_dots);
             if (data.num_dots <= 0) {
-                UIUtils.showError("Need to have a positive number of dots.");
+                showError("Need to have a positive number of dots.");
                 return;
             }
 
-            if (data.dot_format === null) {
+            if (_.isNull(data.dot_format)) {
                 data.dot_format = popup.find(".dot_format select option:first").val();
             }
 
-            if (data.field_type === null) {
+            if (_.isNull(data.field_type)) {
                 data.field_type = popup.find(".field_type select option:first").val();
             }
 
             // save show and initialize controller
 
-            var controller = EditorController.init(Show.create(data));
+            let controller = EditorController.init(Show.create(data));
             controller.saveShow(function() {
-                UIUtils.hidePopup();
+                hidePopup();
             });
         },
     });
