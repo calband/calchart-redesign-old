002f75b6324402b2ef98f80c8143b57df3319ca6
Brandon Chinn <brandonchinn178@gmail.com>
2017-01-21 16:53:33 -0800

    fix slug

--- calchart/calchart/urls.py
+++ calchart/calchart/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^login/$', LoginView.as_view(), name='login'),
     url(r'^logout/$', logout_view, name='logout'),
-    url(r'^editor/(?P<slug>\w+)/$', EditorView.as_view(), name='editor'),
+    url(r'^editor/(?P<slug>.+)/$', EditorView.as_view(), name='editor'),
 
     # endpoints for server-side processing
     url(r'^download/(?P<slug>\w+)\.json$', export),
