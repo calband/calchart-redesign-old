5c8f2ccb06ccb8cbcecd02007a1ca13a6a04006f
Brandon Chinn <brandonchinn178@gmail.com>
2017-03-11 23:43:13 -0800

    remove unnecessary HTMLBuilder.button

--- src/utils/HTMLBuilder.js
+++ src/utils/HTMLBuilder.js
@@ -43,18 +43,6 @@ export default class HTMLBuilder {
     }
 
     /**
-     * Build a <button> element
-     *
-     * @param {string} text -- the text to put in the <button>
-     * @param {string} class -- the class to add to the <button>
-     */
-    static button() {
-        let args = parseArgs(arguments, ["text", "class"]);
-
-        return $("<button>").addClass(args.class).text(args.text);
-    }
-
-    /**
      * Build a <div> element
      *
      * @param {string} class -- the class to add to the <div>
