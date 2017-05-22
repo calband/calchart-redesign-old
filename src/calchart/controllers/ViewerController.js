eefbc8270627e23da663eb8d606fe7412b3f6036
Brandon Chinn <brandonchinn178@gmail.com>
2017-05-22 14:08:53 -0700

    add seek bar

--- src/calchart/controllers/ViewerController.js
+++ src/calchart/controllers/ViewerController.js
@@ -1,6 +1,8 @@
 import ApplicationController from "calchart/ApplicationController";
 import Grapher from "calchart/Grapher";
 
+import { round, roundSmall } from "utils/MathUtils";
+
 /**
  * The controller that stores the state of the viewer application and contains
  * all of the actions that can be run in the viewer page.
@@ -15,6 +17,17 @@ export default class ViewerController extends ApplicationController {
         this._grapher = null;
         this._currSheet = show.getSheets()[0];
         this._currBeat = 0;
+        this._isPlaying = false;
+
+        // cache the cumulative beats for each sheet; i.e. the number of beats
+        // until that sheet
+        let totalBeats = 0;
+        this._cumBeats = show.getSheets().map(sheet => {
+            let curr = totalBeats;
+            totalBeats += sheet.getDuration();
+            return curr;
+        });
+        this._totalBeats = totalBeats;
     }
 
     static get shortcuts() {
@@ -27,11 +40,52 @@ export default class ViewerController extends ApplicationController {
         this._grapher = new Grapher(this._show, $(".viewer"));
         this.refresh();
 
-        // TODO: set up seek bar
+        this._setupSeek();
+
         // TODO: set up buttons
         // TODO: set up clicking dots in graph
         // TODO: set up selecting dots in controls
     }
+
+    /**
+     * Refresh the UI according to the current state of the viewer
+     * and Show.
+     */
+    refresh() {
+        if (_.isUndefined(this._currSheet)) {
+            this._grapher.drawField();
+            return;
+        }
+
+        this._grapher.draw(this._currSheet, this._currBeat);
+        $(".details .sheet").text(this._currSheet.getLabel());
+        let beatNum = this._currBeat === 0 ? "Hup" : this._currBeat;
+        $(".details .beat-num").text(beatNum);
+        $(".details .beat-total").text(this._currSheet.getDuration());
+
+        // refresh seek
+        let beat = this._getCumulativeBeat();
+        let position = $(".seek").width() / this._totalBeats * beat;
+        $(".seek .marker").css("transform", `translateX(${position}px)`);
+    }
+
+    /**** ANIMATION ****/
+
+    /**
+     * Starts animating the show.
+     */
+    play() {
+        // TODO
+    }
+
+    /**
+     * Stops animating the show.
+     */
+    stop() {
+        // TODO
+    }
+
+    /**** CONTROLS ****/
 
     /**
      * Increment the current beat.
@@ -98,20 +152,96 @@ export default class ViewerController extends ApplicationController {
     }
 
     /**
-     * Refresh the UI according to the current state of the viewer
-     * and Show.
+     * Set the current beat, setting the corresponding sheet.
+     *
+     * @param {number} beat - The beat to set.
      */
-    refresh() {
-        if (_.isUndefined(this._currSheet)) {
-            this._grapher.drawField();
-            return;
+    setBeat(beat) {
+        let [currSheet, currBeat] = this._getSheetAndBeat(beat);
+
+        this._currSheet = currSheet;
+        this._currBeat = currBeat;
+        this.refresh();
+    }
+
+    /**
+     * If the show is currently animating, stop the animation. Otherwise,
+     * start the animation.
+     */
+    togglePlay() {
+        if (this._isPlaying) {
+            // TODO: change icon
+            this.stop();
+        } else {
+            // TODO: change icon
+            this.play();
+        }
+    }
+
+    /**
+     * Get the cumulative number of beats for the current sheet and beat
+     *
+     * @return {number}
+     */
+    _getCumulativeBeat() {
+        let index = this._currSheet.getIndex();
+        return this._cumBeats[index] + this._currBeat;
+    }
+
+    /**
+     * Get the sheet and beat for the given cumulative beat number.
+     *
+     * @param {number} beat - The cumulative number of beats
+     * @return {[Sheet, number]}
+     */
+    _getSheetAndBeat(beat) {
+        let sheet = this._show.getSheets()[0];
+
+        while (beat > sheet.getDuration()) {
+            beat -= sheet.getDuration();
+            sheet = sheet.getNextSheet();
         }
 
-        this._grapher.draw(this._currSheet, this._currBeat);
-        $(".details .sheet").text(this._currSheet.getLabel());
-        let beatNum = this._currBeat === 0 ? "Hup" : this._currBeat;
-        $(".details .beat-num").text(beatNum);
-        $(".details .beat-total").text(this._currSheet.getDuration());
+        return [sheet, beat];
+    }
+
+    /**
+     * Set up the seekbar
+     */
+    _setupSeek() {
+        let seek = $(".controls .seek");
+        let marker = seek.find(".marker");
+        let markerRadius = marker.width() / 2;
+        let seekLeft = seek.offset().left;
+        let seekWidth = seek.width();
+        let interval = seekWidth / this._totalBeats;
+
+        let updateSeek = pageX => {
+            let prev = marker.offset().left;
+
+            // snap to beat
+            let x = _.clamp(pageX - seekLeft - markerRadius, 0, seekWidth);
+            let beat = roundSmall(round(x, interval) / interval);
+
+            // don't redraw screen if the beat didn't change
+            if (x !== prev) {
+                this.setBeat(beat);
+            }
+        };
+
+        seek.mousedown(e => {
+            e.preventDefault();
+            updateSeek(e.pageX);
+
+            $(document).on({
+                "mousemove.seek": e => {
+                    updateSeek(e.pageX);
+                },
+                "mouseup.seek": e => {
+                    $(document).off(".seek");
+                },
+            });
+        });
     }
 }
 
