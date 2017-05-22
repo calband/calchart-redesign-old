6246182e0d33f18f64b20208dc4db0dd3544b763
Brandon Chinn <brandonchinn178@gmail.com>
2017-05-22 13:40:20 -0700

    more viewer

--- src/calchart/controllers/ViewerController.js
+++ src/calchart/controllers/ViewerController.js
@@ -13,8 +13,8 @@ export default class ViewerController extends ApplicationController {
         super(show);
 
         this._grapher = null;
-        this._currSheet = null;
-        this._currBeat = null;
+        this._currSheet = show.getSheets()[0];
+        this._currBeat = 0;
     }
 
     static get shortcuts() {
@@ -24,170 +24,95 @@ export default class ViewerController extends ApplicationController {
     init() {
         super.init();
 
-        // let workspace = $(".workspace");
-        // this._grapher = new Grapher(this._show, workspace, {
-        //     boundDots: true,
-        //     drawYardlineNumbers: true,
-        //     draw4Step: true,
-        //     drawDotType: true,
-        //     expandField: true,
-        //     showLabels: true,
-        //     zoom: 1,
-        // });
-        // this._grapher.drawField();
+        this._grapher = new Grapher(this._show, $(".viewer"));
+        this.refresh();
 
-        // let sheet = _.first(this._show.getSheets());
-        // if (sheet) {
-        //     this.loadSheet(sheet);
-        // }
+        // TODO: set up seek bar
+        // TODO: set up buttons
+        // TODO: set up clicking dots in graph
+        // TODO: set up selecting dots in controls
     }
 
-    // /**
-    //  * Go to the zero-th beat of the sheet.
-    //  */
-    // firstBeat() {
-    //     this._currBeat = 0;
-    //     this.refresh("grapher", "context");
-    // }
+    /**
+     * Increment the current beat.
+     */
+    nextBeat() {
+        this._currBeat++;
 
-    // /**
-    //  * @param {?Sheet} The currently active sheet, or null if there is no active Sheet (i.e.
-    //  *   there are no Sheets in the Show).
-    //  */
-    // getActiveSheet() {
-    //     return this._activeSheet;
-    // }
+        if (this._currBeat === this._currSheet.getDuration()) {
+            this.nextSheet();
+        } else {
+            this.refresh();
+        }
+    }
 
-    // /**
-    //  * @param {int}
-    //  */
-    // getCurrentBeat() {
-    //     return this._currBeat;
-    // }
+    /**
+     * Increment the current sheet.
+     */
+    nextSheet() {
+        let nextSheet = this._currSheet.getNextSheet();
 
-    // /**
-    //  * Go to the last beat of the sheet.
-    //  */
-    // lastBeat() {
-    //     this._currBeat = this._activeSheet.getDuration();
-    //     this.refresh("grapher", "context");
-    // }
+        if (_.isNull(nextSheet)) {
+            // currently on last sheet; go to last beat
+            this._currBeat = this._currSheet.getDuration() - 1;
+        } else {
+            this._currBeat = 0;
+            this._currSheet = this._currSheet.getNextSheet();
+        }
 
-    // /**
-    //  * Increment the current beat.
-    //  */
-    // nextBeat() {
-    //     this._currBeat++;
-    //     let duration = this._activeSheet.getDuration();
+        this.refresh();
+    }
 
-    //     if (this._currBeat > duration) {
-    //         this._currBeat = duration;
-    //     } else {
-    //         this.refresh("grapher", "context");
-    //     }
-    // }
+    /**
+     * Decrement the current beat.
+     */
+    prevBeat() {
+        this._currBeat--;
 
-    // /**
-    //  * Decrement the current beat.
-    //  */
-    // prevBeat() {
-    //     this._currBeat--;
+        if (this._currBeat < 0) {
+            let prevSheet = this._currSheet.getPrevSheet();
+            if (_.isNull(prevSheet)) {
+                // currently on first sheet; go to first beat
+                this._currBeat = 0;
+            } else {
+                this._currBeat = prevSheet.getDuration() - 1;
+                this._currSheet = prevSheet;
+            }
+        }
 
-    //     if (this._currBeat < 0) {
-    //         this._currBeat = 0;
-    //     } else {
-    //         this.refresh("grapher", "context");
-    //     }
-    // }
+        this.refresh();
+    }
 
-    // /**
-    //  * Refresh the UI according to the current state of the editor
-    //  * and Show.
-    //  *
-    //  * @param {...String} [targets=all] - The elements to refresh.
-    //  *   Elements that can be refreshed:
-    //  *     - all
-    //  *     - sidebar (default all)
-    //  *     - sidebarPreviews (default sidebar)
-    //  *     - grapherClear: empties grapher before refreshing
-    //  *     - grapher (default all || grapherClear)
-    //  *     - context (default all)
-    //  */
-    // refresh(...targets) {
-    //     // targets to refresh
-    //     let refresh = {};
-    //     refresh.all = targets.includes("all") || targets.length === 0;
-    //     refresh.sidebar = refresh.all || targets.includes("sidebar");
-    //     refresh.sidebarPreviews = refresh.sidebar || targets.includes("sidebarPreviews");
-    //     refresh.grapherClear = targets.includes("grapherClear");
-    //     refresh.grapher = refresh.all || refresh.grapherClear || targets.includes("grapher");
-    //     refresh.context = refresh.all || targets.includes("context");
+    /**
+     * Decrement the current sheet.
+     */
+    prevSheet() {
+        this._currBeat = 0;
+        let prevSheet = this._currSheet.getPrevSheet();
 
-    //     let _this = this;
-    //     let sidebar = $(".sidebar");
+        if (!_.isNull(prevSheet)) {
+            this._currSheet = prevSheet;
+        }
 
-    //     if (refresh.sidebar) {
-    //         sidebar.empty();
-    //         this._show.getSheets().forEach(sheet => {
-    //             let label = HTMLBuilder.span(sheet.getLabel(), "label");
+        this.refresh();
+    }
 
-    //             let preview = HTMLBuilder.div("preview");
-    //             let $sheet = HTMLBuilder
-    //                 .div("stuntsheet", [label, preview], sidebar)
-    //                 .data("sheet", sheet);
+    /**
+     * Refresh the UI according to the current state of the viewer
+     * and Show.
+     */
+    refresh() {
+        if (_.isUndefined(this._currSheet)) {
+            this._grapher.drawField();
+            return;
+        }
 
-    //             if (sheet === this._activeSheet) {
-    //                 $sheet.addClass("active");
-    //             }
-    //         });
-    //         sidebar.find(".stuntsheet.active").scrollIntoView({
-    //             margin: 10,
-    //         });
-    //     }
-
-    //     if (refresh.sidebarPreviews) {
-    //         sidebar.find(".stuntsheet").each(function() {
-    //             let sheet = $(this).data("sheet");
-    //             let preview = $(this).find(".preview");
-
-    //             // field preview
-    //             let grapher = new Grapher(_this._show, preview, {
-    //                 drawOrientation: false,
-    //                 drawYardlines: false,
-    //                 fieldPadding: 5,
-    //             })
-    //             grapher.draw(sheet);
-    //         });
-    //     }
-
-    //     // refresh grapher
-    //     if (refresh.grapher) {
-    //         let selectedDots = this._selectedDots;
-    //         if (refresh.grapherClear) {
-    //             selectedDots = this.getSelectedDots().map(dot => dot.id);
-    //             this._grapher.clear();
-    //         }
-
-    //         if (this._activeSheet) {
-    //             this._grapher.draw(this._activeSheet, this._currBeat);
-    //         } else {
-    //             this._grapher.drawField();
-    //         }
-
-    //         // this._selectedDots will refer to stale dots after calling grapher.clear()
-    //         if (refresh.grapherClear) {
-    //             selectedDots = this._grapher.getDots(selectedDots);
-    //         }
-
-    //         this._grapher.selectDots(selectedDots);
-    //         this._selectedDots = selectedDots;
-    //     }
-
-    //     // refresh context
-    //     if (refresh.context && this._context) {
-    //         this._context.refresh();
-    //     }
-    // }
+        this._grapher.draw(this._currSheet, this._currBeat);
+        $(".details .sheet").text(this._currSheet.getLabel());
+        let beatNum = this._currBeat === 0 ? "Hup" : this._currBeat;
+        $(".details .beat-num").text(beatNum);
+        $(".details .beat-total").text(this._currSheet.getDuration());
+    }
 }
 
 let EditorShortcuts = {
@@ -195,4 +120,5 @@ let EditorShortcuts = {
     "right": "nextBeat",
     "shift+left": "prevSheet",
     "shift+right": "nextSheet",
+    "space": "togglePlay", // TODO: add space to shortcut keys
 };
