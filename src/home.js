e1d7fcaecee046574b391d4c0c6f94a309564275
Brandon Chinn <brandonchinn178@gmail.com>
2017-05-31 21:35:43 -0700

    clean home.js

--- src/home.js
+++ src/home.js
@@ -3,10 +3,5 @@ import HomeController from "controllers/HomeController";
 import "utils/jquery";
 
 $(function() {
-    if (_.isNull(window.show)) {
-        alert("This show isn't set up yet!");
-        return;
-    }
-
     HomeController.init(null);
 });
