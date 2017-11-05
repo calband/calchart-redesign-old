42d094564718e3b77fbb9e2b63295af489c1b658
Brandon Chinn <brandonchinn178@gmail.com>
2017-11-05 13:25:12 -0800

    Start adding Django tests

---
+++ calchart/tests/test_home.py
@@ -0,0 +1,61 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from datetime import timedelta
+
+from base.models import User, Show
+from base.views import CalchartView
+from utils.testing import ActionsTestCase, RequestFactory
+
+class TabsTestCase(TestCase):
+    """Test retrieving tabs for the home page."""
+
+    def test_get_tabs_members_only_user(self):
+        request = RequestFactory.GET(members_only=True)
+        response = CalchartView.as_view()(request)
+
+        tabs = response.context_data['tabs']
+        self.assertEqual(len(tabs), 2)
+        self.assertEqual(tabs[0][0], 'band')
+
+    # TODO: test get_tabs not members_only_user
+    # TODO: test get_tab band members_only_user
+    # TODO: test get_tab band not members_only_user
+    # TODO: test get_tab owned
+
+class CreateShowTestCase(ActionsTestCase):
+    """Test the create_show action."""
+
+    def assertCreateShow(self, is_band, members_only, final_is_band):
+        response = self.do_action('create_show', {
+            'name': 'Foo',
+            'isBand': is_band,
+        }, members_only=members_only)
+        
+        self.assertTrue(Show.objects.filter(name='Foo').exists())
+        show = Show.objects.get(name='Foo')
+        self.assertEqual(show.is_band, final_is_band)
+        self.assertEqual(response.get('slug'), 'foo')
+
+    def test_create_show_band_members_only(self):
+        self.assertCreateShow(True, True, True)
+
+    def test_create_show_band_calchart(self):
+        # if Calchart-only user, Show should never be is_band
+        self.assertCreateShow(True, False, False)
+
+    def test_create_show_not_band_members_only(self):
+        self.assertCreateShow(False, True, False)
+
+    def test_create_show_not_band_calchart(self):
+        self.assertCreateShow(False, False, False)
+
+    # TODO: test create Show twice errors
+
+class PublishShowTestCase(ActionsTestCase):
+    """Test the publish_show action."""
+
+    # TODO: test publish initialized show
+    # TODO: test unpublish initialized show
+    # TODO: test publish un-initialized show
+    # TODO: test unpublish un-initialized show
---
+++ calchart/utils/testing.py
@@ -0,0 +1,74 @@
+"""Utilities for testing."""
+
+from django.test import TestCase, RequestFactory as DjangoRequestFactory
+
+import json
+from unittest import mock
+
+from base.models import User
+from base.views import CalchartView
+
+class _RequestFactory(object):
+    """A new RequestFactory that augments Django's built-in RequestFactory."""
+
+    _factory = DjangoRequestFactory()
+
+    def get_user(self, members_only):
+        """Get a Members Only or Calchart-only user for a request."""
+        if members_only:
+            # use superuser to avoid API call to check-committee
+            return User.objects.create_superuser(username='foo', password='foo', email='')
+        else:
+            return User.objects.create(username='bar')
+
+    def GET(self, data=None, members_only=False):
+        if data is None:
+            data = {}
+
+        request = self._factory.get('/', data)
+        request.user = self.get_user(members_only=members_only)
+        return request
+
+    def POST(self, data=None, members_only=False):
+        if data is None:
+            data = {}
+
+        request = self._factory.post('/', data)
+        request.user = self.get_user(members_only=members_only)
+        return request
+
+RequestFactory = _RequestFactory()
+
+class ActionsTestCase(TestCase):
+    """A TestCase for testing POST actions."""
+
+    def do_action(self, action, data, **kwargs):
+        """Run the given action and return the data sent back."""
+        request = RequestFactory.POST({
+            'action': action,
+            'data': json.dumps(data),
+        }, **kwargs)
+        response = CalchartView.as_view()(request)
+        self.assertEqual(response.status_code, 200)
+        return json.loads(response.content)
+
+def mock_endpoint(endpoint, data):
+    """
+    Mock any calls to utils.api.call_endpoint, returning the given data.
+
+    Usage:
+    with mock_endpoint('check_committee', { 'has_committee': True }):
+        response = call_endpoint('check_comittee', user)
+        self.assertTrue(response['has_committee'])
+    """
+    def mock_call(url, *args, **kwargs):
+        response = mock.MagicMock()
+        if url.split('/')[-2] == endpoint:
+            response.json.return_value = data
+        else:
+            response.json.return_value = None
+        return response
+
+    mocked_requests = mock.MagicMock()
+    mocked_requests.get = mocked_requests.post = mock_call
+    return mock.patch('utils.api.requests', new=mocked_requests)
