46746de9495e38b5b85943825cf9f6ef32ef761c
Brandon Chinn <brandonchinn178@gmail.com>
2018-01-07 19:05:18 -0800

    Fix test

--- test/App.spec.js
+++ test/App.spec.js
@@ -1,13 +1,11 @@
 import { shallow } from 'vue-test-utils';
 
 import App from 'App';
-import { $store } from 'test/utils';
+import { addRouter, addStore } from 'test/utils';
 
 describe('App', () => {
     it('does not show messages on init', () => {
-        let wrapper = shallow(App, {
-            mocks: { $store },
-        });
+        let wrapper = addRouter(addStore(shallow))(App);
         expect(wrapper.contains('ul.messages')).toBe(false);
     });
 });
