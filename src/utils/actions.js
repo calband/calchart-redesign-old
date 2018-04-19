d1f8e93761f16594d0812fec9f8ff0c187e640b4
Brandon Chinn <brandonchinn178@gmail.com>
2018-04-19 08:09:56 -0700

    Add parseAction function

---
+++ src/utils/actions.js
@@ -0,0 +1,63 @@
+/**
+ * @file Functions for parsing editor actions.
+ */
+
+import { attempt, includes, isError, isNull, split, trim } from 'lodash';
+
+/**
+ * A helper to parse arguments. Supports any JSON strings, with the following
+ * syntactic helpers:
+ *  - a string does not need quotes: `foo(bar)` -> `foo('bar')`
+ *  - an object can use python notation:
+ *    `foo(bar=1, foo=2)` -> `foo({ bar: 1, foo: 2 })
+ *
+ * @param {String} arg
+ * @return {*}
+ */
+function parseArg(arg) {
+    let json = attempt(() => JSON.parse(arg));
+    if (!isError(json)) {
+        return json;
+    } else if (includes(arg, '=')) {
+        let o = {};
+        split(arg, ',').forEach(pair => {
+            let [key, val] = split(pair, '=');
+            o[trim(key)] = parseArg(val);
+        });
+        return o;
+    } else {
+        // unquoted string
+        return arg;
+    }
+}
+
+/**
+ * Parse the given function name.
+ *
+ * @param {string} name - The function name, in one of the following formats:
+ *   - 'name': the name of the function, without arguments specified
+ *   - 'name(arg)': the name of the function, run with the given argument.
+ *     See parseArg for the format of the argument.
+ * @return {Object}
+ *   - {String} name
+ *   - {*} data
+ */
+export default function parseAction(name) {
+    let actionMatch = name.match(/^(\w+)(\((.+)\))?$/);
+
+    if (isNull(actionMatch)) {
+        throw new Error(`Action name in an invalid format: ${name}`);
+    }
+
+    let actionName = actionMatch[1];
+    let actionArg = null;
+
+    if (actionMatch[2]) {
+        actionArg = parseArg(actionMatch[3]);
+    }
+
+    return {
+        name: actionName,
+        data: actionArg,
+    };
+}
