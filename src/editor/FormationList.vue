5a4fe34e465de08780b1fd1fb5f8a9c119207fad
Brandon Chinn <brandonchinn178@gmail.com>
2018-04-28 10:45:50 -0700

    Implement FormationList

---
+++ src/editor/FormationList.vue
@@ -0,0 +1,42 @@
+<docs>
+The sidebar containing a list of formations for the editor.
+</docs>
+
+<template>
+    <div>
+        <div class="scrollable">
+            TODO
+        </div>
+        <button :click="null" class="add-formation">Add</button>
+    </div>
+</template>
+
+<script>
+import Formation from 'calchart/Formation';
+import { validateType } from 'utils/types';
+
+export default {
+    props: {
+        formations: {
+            // The formations to show in the sidebar
+            type: Array,
+            required: true,
+            validator: validateType({
+                _type: 'array',
+                _wraps: Formation,
+            }),
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.scrollable {
+    height: calc(100% - #{$toolbar-height});
+}
+
+button.add-formation {
+    margin: 5px auto;
+    display: block;
+}
+</style>
