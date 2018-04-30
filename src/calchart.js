e6f80a6245c7002c4393a7b71c1ca40610f0165b
Brandon Chinn <brandonchinn178@gmail.com>
2018-04-30 08:16:26 -0700

    Add popup code

--- src/calchart.js
+++ src/calchart.js
@@ -3,12 +3,14 @@ import VueContextMenu from 'vue-ctxmenu';
 
 import App from 'App';
 import FormsPlugin from 'forms';
+import PopupPlugin from 'popups';
 import initRouter from 'router';
 import initStore from 'store';
 import { ConstantsPlugin } from 'utils/vue';
 
 Vue.use(ConstantsPlugin);
 Vue.use(FormsPlugin);
+Vue.use(PopupPlugin);
 Vue.use(VueContextMenu);
 
 let CalchartApp = Vue.extend(App);
