7213a5878ccc0fe1d5c2a135b2f68383fcd84c0d
Brandon Chinn <brandonchinn178@gmail.com>
2018-05-17 18:25:46 -0700

    Add cypress tests for formation list

--- cypress/integration/formation.js
+++ cypress/integration/formation.js
@@ -2,19 +2,53 @@
  * @file Integration tests related to Formations.
  */
 
+function createFormation(formationName) {
+    cy.get('[data-cy=add-formation]').click();
+    cy.get('.popup').should('exist');
+
+    cy.get('.popup input[name=name]').type(formationName);
+    cy.get('.popup [data-cy=popup-submit]').click();
+}
+
+// The graph preview within the Formation.
+const formationGraph = '[data-cy=formation-graph]';
+
 describe('Formation', () => {
     it('creates a new Formation', () => {
         cy.gotoTestShow();
         cy.get('[data-cy=formation]').should('not.exist');
 
-        cy.get('[data-cy=add-formation]').click();
-        cy.get('.popup').should('exist');
+        createFormation('Formation 1');
 
-        let formationName = 'Formation 1';
-        cy.get('.popup input[name=name]').type(formationName);
-        cy.get('.popup [data-cy=popup-submit]').click();
+        cy.get('[data-cy=formation]').should('exist').within(() => {
+            cy.get(formationGraph).should('have.class', 'active');
+        });
+    });
 
-        cy.get('[data-cy=formation]').should('exist')
-            .get('[data-cy=formation-graph]').should('have.class', 'active');
+    it('switches Formations', () => {
+        cy.gotoTestShow();
+        cy.get('[data-cy=formation]').should('not.exist');
+
+        let form1 = 'Formation 1';
+        let form2 = 'Formation 2';
+
+        createFormation(form1);
+        createFormation(form2);
+        cy.contains('[data-cy=formation]', form2).should('exist')
+            .within(() => {
+                cy.get(formationGraph).should('have.class', 'active');
+            })
+
+        cy.contains('[data-cy=formation]', form1).within(() => {
+            cy.get(formationGraph).should('not.have.class', 'active')
+                .click()
+                .should('have.class', 'active');
+        })
+
+        cy.contains('[data-cy=formation]', form2).within(() => {
+            cy.get(formationGraph).should('not.have.class', 'active')
+                .click()
+                .should('have.class', 'active');
+        })
     });
 });
